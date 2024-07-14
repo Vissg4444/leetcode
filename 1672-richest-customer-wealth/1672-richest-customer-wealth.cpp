afa0ba5767b0246e1422ba5b1082faa5015472b0
Vissg <vissg4444@gmail.com>
2024-07-15 01:46:34 +0700

    Time: 7 ms (26.11%), Space: 10.5 MB (7.48%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) 
     {
-        int wealth = 0, temp = 0;
+        int wealth = 0;
         for(int i = 0; i < accounts.size(); i++)
         {
+            int temp = 0;
             for(int j = 0; j < accounts[i].size(); j++)
             {
                 temp += accounts[i][j];
             }
-            wealth = temp > wealth ? temp : wealth;
-            temp = 0;
+            wealth = max(temp, wealth);
+
         }
         return wealth;
     }
