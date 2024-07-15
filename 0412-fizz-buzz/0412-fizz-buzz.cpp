404f0ce0ebd641e9e4db0ba6c672746ee1030804
Vissg <vissg4444@gmail.com>
2024-07-15 13:22:48 +0700

    Time: 5 ms (33.08%), Space: 11 MB (5.85%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+vector<string> fizzBuzz(int n)
+{
+   vector<string> out(n);
+   for ( int i = 1; i <= n; i++ )
+   {
+        if ( i % 3 == 0 and i % 5 == 0 )
+            out[i - 1] = "FizzBuzz";
+        else if (i % 3 == 0 and i % 5 != 0)
+            out[i - 1] = "Fizz";
+        else if (i % 3 != 0 and i % 5 == 0)
+            out[i - 1] = "Buzz";
+        else
+         out[i - 1] = to_string(i);
+   }
+   return out;
+}
+};
