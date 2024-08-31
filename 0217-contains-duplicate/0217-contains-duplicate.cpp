2669c2c81022b8747fd769e111cc9a26865033b8
Vissg <vissg4444@gmail.com>
2024-09-01 04:50:55 +0700

    Time: 118 ms (19.15%), Space: 74.5 MB (27.38%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,9 +4,8 @@ public:
         map<int, int> dict;
         for(int i = 0; i < size(nums); i++)
         {
+            if (dict.count(nums[i]) != 0) return true;
             dict[nums[i]]++;
-            if (dict[nums[i]] > 1)
-                return true;
         }
         
         return false;
