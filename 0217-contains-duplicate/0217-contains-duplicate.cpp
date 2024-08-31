52feae5d7721d8488ed4c7b6876f1212232d807c
Vissg <vissg4444@gmail.com>
2024-09-01 04:47:48 +0700

    Time: 142 ms (6.29%), Space: 74.7 MB (24.6%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int, char> dict;
+        map<int, int> dict;
         for(int i : nums)
         {
             dict[i]++;
-            if (dict[i] >= 1)
+            if (dict[i] > 1)
                 return true;
         }
         
