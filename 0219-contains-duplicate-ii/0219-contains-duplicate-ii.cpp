97ad4efbb4ca5bd06060b734ffb8f22e3c88f341
Vissg <vissg4444@gmail.com>
2024-09-03 18:03:10 +0700

    Time: 119 ms (61.42%), Space: 81.2 MB (29.72%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -3,7 +3,7 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_map<int, int> dict;
         for (int i = 0; i < size(nums); i++) {
-            if (dict.count(nums[i])) {
+            if (dict.contains(nums[i])) {
                 {
                     if (i - dict[nums[i]] <= k)
                         return true;
