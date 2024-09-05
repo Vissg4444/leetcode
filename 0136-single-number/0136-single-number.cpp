87c773d66fd54d3cd71168e56f38a108480b87cd
Vissg <vissg4444@gmail.com>
2024-09-06 05:14:30 +0700

    Time: 15 ms (52.89%), Space: 19.5 MB (84%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for(int i = 0; i < size(nums); i++)
+        {
+            result ^= nums[i];
+        }
+        return result;
+    }
+};
