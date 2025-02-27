811fcac9949a6730cc972cfda0ccba1c39ba55d8
Vissg <vissg4444@gmail.com>
2025-02-27 20:10:02 +0700

    Time: 8 ms (81.07%), Space: 11.9 MB (24.39%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -3,23 +3,24 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
-    bool hasCycle(ListNode *head) 
-    {
-        if (!head) return false;
-        ListNode *fast = head->next, *slow = head;
-        while(fast and slow) 
+    bool hasCycle(ListNode* head) {
+        if(!head) return false;
+        ListNode* fast = head->next;
+        ListNode* slow = head;
+
+        while(fast and fast->next)
         {
-            if (fast == slow)
-                return true;
+            if(fast == slow) return true;
+            fast = fast->next->next;
             slow = slow->next;
-            fast = fast->next;
-            if (fast)
-                fast = fast->next;
         }
         return false;
     }
