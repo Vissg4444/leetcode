b3ba36fda1358296fba04866ae42b58462aea32c
Vissg <vissg4444@gmail.com>
2025-08-13 18:07:28 +0700

    Time: 3 ms (14.11%), Space: 28 MB (47.94%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -9,9 +9,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
+
     int maxPathSum(TreeNode* root) {
-        
+        int out = root->val;
+        DFS(root, out);
+        return out;
+
+    }
+
+    int DFS(TreeNode* root, int& out)
+    {
+        if (!root) return 0;
+
+        int left = max(DFS(root->left, out), 0);
+        int right = max(DFS(root->right, out), 0);
+
+        out = max(out, root->val + left + right);
+
+        return root->val + max(left, right);
+
     }
 };
