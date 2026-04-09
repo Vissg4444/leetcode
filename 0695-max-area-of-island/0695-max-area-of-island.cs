7886ce485d407ab413e7d2cb156718d0f46a39e2
Vissg <vissg4444@gmail.com>
2026-04-10 00:37:30 +0700

    Time: 4 ms (69.11%), Space: 44.3 MB (100%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    public int MaxAreaOfIsland(int[][] grid) {
+        int maxarea = 0;
+        for (int i = 0; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid[0].Length; j++)
+            {
+                
+                if (grid[i][j] != 0)
+                {
+                    maxarea = Math.Max(maxarea, DFS(grid, i, j, 1));
+                }
+            }
+        }
+        return maxarea;
+    }
+    public int DFS(int[][] grid, int i, int j, int area)
+    {
+        if (
+        i >= grid.Length 
+        || i < 0 
+        || j >= grid[0].Length 
+        || j < 0 
+        || grid[i][j] == 0
+        )
+        {
+            return 0;
+        }
+
+        grid[i][j] = 0;
+        int r = DFS(grid, i + 1, j, area + 1);
+        int l = DFS(grid, i - 1, j, area + 1);
+        int t = DFS(grid, i, j + 1, area + 1);
+        int d = DFS(grid, i, j - 1, area + 1);
+        return 1 + r + l + t + d;
+
+    }
+}
