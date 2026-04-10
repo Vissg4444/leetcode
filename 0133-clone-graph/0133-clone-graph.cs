3a2d0da625188629e19096a6e6264f9681db50a1
Vissg <vissg4444@gmail.com>
2026-04-10 22:38:25 +0700

    Time: 135 ms (39.16%), Space: 48.2 MB (15.46%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cs
@@ -0,0 +1,23 @@
+public class Solution
+{
+    Dictionary<Node, Node> cloned = new Dictionary<Node, Node>();
+    public Node CloneGraph(Node node)
+    {
+        return DFS(node);
+    }
+    public Node DFS(Node node)
+    {
+        if (node == null)
+            return null;
+        if (cloned.ContainsKey(node)) 
+            return cloned[node];
+
+        Node NewNode = new Node(node.val);
+        cloned[node] = NewNode;
+
+        foreach (Node neighbor in node.neighbors)
+            NewNode.neighbors.Add(DFS(neighbor));
+
+        return NewNode;
+    }
+}
